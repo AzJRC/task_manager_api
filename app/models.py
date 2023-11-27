fc6442a4b56f730d5ea01a9cc031bb0b3132d98e
Alejandro <rodajrc@gmail.com>
2023-11-27 11:48:41 -0600

    user groups models and schemas

--- app/models.py
+++ app/models.py
@@ -1,8 +1,26 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, text
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Table, text, Enum
+from sqlalchemy.orm import relationship, declarative_base
 from .database import Base
 
 
+dec_base = declarative_base()
+
+
+# Association tables
+
+
+group_roles = Table("group_roles", Base.metadata,
+                    Column("id", Integer, primary_key=True),
+                    Column("role", String, nullable=False)) 
+
+
+user_group_members = Table("user_group_members", Base.metadata, 
+                           Column("user_id", Integer, ForeignKey("users.id"), primary_key=True), 
+                           Column("group_id", Integer, ForeignKey("user_groups.id"), primary_key=True),
+                           Column("role_id", Integer, ForeignKey("group_roles.id"), nullable=False))
+
+
+# Main tables
 class UsersTable(Base):
     __tablename__ = "users"
 
@@ -10,22 +28,52 @@ class UsersTable(Base):
     username = Column(String, nullable=False, unique=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    creation = Column(DateTime(timezone=True), nullable=False, server_default=text("now()"))
-    valid_email = Column(Boolean, nullable=False, server_default=text("FALSE")) # email validation
-    user_state = Column(Boolean, nullable=False, server_default=text("TRUE")) # user_state -> True: Active, False: Inactive (For irreversible user deletion)
+    user_creation = Column(DateTime(timezone=True), nullable=False, server_default=text("now()"))
+    valid_email = Column(Boolean, nullable=False, server_default=text("FALSE"))  # email validation # user_state -> True: Active, False: Inactive (For irreversible user deletion)
+    user_state = Column(Boolean, nullable=False, server_default=text("TRUE"))
 
-    tasks = relationship("TasksTable", back_populates="users")
+    tasks = relationship("TasksTable", back_populates="task_owner") 
+    user_group = relationship("UserGroupsTable", back_populates="user_group_owner")
+    task_group = relationship("TaskGroupsTable", back_populates="task_group_owner")
+    
+    #secondary relationships
+    group_member = relationship("UserGroupsTable", secondary=user_group_members, back_populates="members")
+
+
+class UserGroupsTable(Base):
+    __tablename__ = "user_groups"
+
+    id = Column(Integer, primary_key=True, index=True)
+    group_name = Column(String, nullable=False, unique=True)
+    group_description = Column(String, nullable=True)
+    group_creation = Column(DateTime(timezone=True), nullable=False, server_default=text("now()"))
+    group_owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    user_group_owner = relationship("UsersTable", back_populates="user_group")
+    
+    #secondary relationships
+    members = relationship("UsersTable", secondary=user_group_members, back_populates="group_member")
 
 
 class TasksTable(Base):
     __tablename__ = "tasks"
+
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    title = Column(String, nullable=False, unique=True)
     description = Column(String, nullable=True)
-    content = Column(String, nullable=False)
-    active = Column(Boolean, nullable=False, server_default=text("TRUE"))
-    creation = Column(DateTime(timezone=True), nullable=False, server_default=text("now()"))
-    task_owner = Column(Integer, ForeignKey("users.id"), nullable=False)
+    task_creation = Column(DateTime(timezone=True), nullable=False, server_default=text("now()"))
+    task_owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
-    users = relationship("UsersTable", back_populates="tasks")
+    task_owner = relationship("UsersTable", back_populates="tasks")
 
+
+class TaskGroupsTable(Base):
+    __tablename__ = "task_groups" 
+
+    id = Column(Integer, primary_key=True, index=True)
+    group_name = Column(String, nullable=False, unique=True)
+    group_description = Column(String, nullable=True)
+    group_creation = Column(DateTime(timezone=True), nullable=False, server_default=text("now()"))
+    group_owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    task_group_owner = relationship("UsersTable", back_populates="task_group")
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,9 @@
 import datetime
-from typing import Optional
+from typing import Any, ForwardRef, List, Optional
 from pydantic import BaseModel, EmailStr
+from .database import Base
+
+from app.models import UserGroupsTable, UsersTable
 
 # Token schemas (For login)
 
@@ -18,12 +21,30 @@ class getTokenData(BaseModel):
     class Config:
         from_attributes: True
 
+
+# Group schemas
+
+class getOwnerInfo(BaseModel):
+    username: str
+    email: EmailStr
+    class Config:
+        from_attributes: True
+
+
+class returnUserGroupInformation(BaseModel):
+    group_name: str
+    group_creation: datetime.datetime
+    user_group_owner: getOwnerInfo
+    class Config:
+        from_attributes: True
+
+
 # User schemas
 
 class getUser(BaseModel):
     username: str
-    email: EmailStr
-    state: Optional[str] = "active"
+    email: EmailStr 
+    state: str
 
     class Config:
         from_attributes: True
@@ -58,10 +79,11 @@ class returnCurrentUser(BaseModel):
     id: int
     username: str
     email: EmailStr
-    creation: datetime.datetime
+    user_creation: datetime.datetime
     valid_email: bool
     user_state: bool
-
+    group_member: Optional[List[returnUserGroupInformation]] = None
+ 
     class Config:
         from_attributes: True
     
